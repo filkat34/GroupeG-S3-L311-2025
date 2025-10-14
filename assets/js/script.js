18ded9e378da4b3fcc587303909af11b62b4e114
mathildechauvet <mathildechauvet@outlook.fr>
2025-10-14 16:17:21 +0200

    ajout accolade ouverte

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,29 +4,29 @@ window.addEventListener("DOMContentLoaded", (event) => {
     init();
 
     function init() {
-        document.querySelector('.button').addEventListener('click', (event) =>
+        document.querySelector('.button').addEventListener('click', (event) => {
             is_run = !is_run;
-        ((' ' + document.querySelector('.button').className + ' ').indexOf('pause') > -1) ? document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class').replace(' pause', '')) : document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class') + ' pause');
-    })
+            ((' ' + document.querySelector('.button').className + ' ').indexOf('pause') > -1) ? document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class').replace(' pause', '')) : document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class') + ' pause');
+        })
 
-setInterval(function () {
-    if (is_run) {
-        let oDate = new Date();
-        document.querySelector('#hours').innerHTML = adjustTimer(oDate.getHours());
-        document.querySelector('#minutes').innerHTML = adjustTimer(oDate.getMinutes());
-        document.querySelector('#seconds').innerHTML = adjustTimer(oDate.getSeconds());
+        setInterval(function () {
+            if (is_run) {
+                let oDate = new Date();
+                document.querySelector('#hours').innerHTML = adjustTimer(oDate.getHours());
+                document.querySelector('#minutes').innerHTML = adjustTimer(oDate.getMinutes());
+                document.querySelector('#seconds').innerHTML = adjustTimer(oDate.getSeconds());
 
-        document.querySelector('body').style.background = randomHexColor(document.querySelector('#hours').innerHTML, document.querySelector('#minutes').innerHTML, document.querySelector('#seconds').innerHTML);
+                document.querySelector('body').style.background = randomHexColor(document.querySelector('#hours').innerHTML, document.querySelector('#minutes').innerHTML, document.querySelector('#seconds').innerHTML);
 
+            }
+        }, 1000);
     }
-}, 1000);
+
+    function adjustTimer(timer) {
+        (timer < 10 ? '0' + timer : timer);
     }
 
-function adjustTimer(timer) {
-    (timer < 10 ? '0' + timer : timer);
-}
-
-function randomHexColor(x, y) {
-    return "rgb(" + Math.floor(x / 100 * 256) + "," + Math.floor(y / 100 * 256) + "," + Math.floor(z / 100 * 256) + ")";
-}
+    function randomHexColor(x, y) {
+        return "rgb(" + Math.floor(x / 100 * 256) + "," + Math.floor(y / 100 * 256) + "," + Math.floor(z / 100 * 256) + ")";
+    }
 });
