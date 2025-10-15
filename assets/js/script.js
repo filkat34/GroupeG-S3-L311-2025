895c43005f5dcdc856c59bf54c0d300aa440876f
KamoGuillon <139084497+Spaghette5@users.noreply.github.com>
2025-10-15 05:33:12 +0200

    Indentation du code, changement de la couleur du fond, ajout du reset CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,49 @@
 window.addEventListener("DOMContentLoaded", () => {
-    let isRunning = true;
-    const button = document.querySelector('.controls .button');
-    const hoursElement = document.querySelector('#hours');
-    const minutesElement = document.querySelector('#minutes');
-    const secondsElement = document.querySelector('#seconds');
+  let isRunning = true;
+  const button = document.querySelector(".controls .button");
+  const hoursElement = document.querySelector("#hours");
+  const minutesElement = document.querySelector("#minutes");
+  const secondsElement = document.querySelector("#seconds");
 
-    init();
+  init();
 
-    function init() {
-        button?.addEventListener('click', toggleRunState);
-        button?.classList.toggle('pause', isRunning);
+  function init() {
+    button?.addEventListener("click", toggleRunState);
+    button?.classList.toggle("pause", isRunning);
+    updateClock();
+    setInterval(() => {
+      if (isRunning) {
         updateClock();
-        setInterval(() => {
-            if (isRunning) {
-                updateClock();
-            }
-        }, 1000);
-    }
+      }
+    }, 1000);
+  }
 
-    function toggleRunState() {
-        isRunning = !isRunning;
-        button?.classList.toggle('pause', isRunning);
-    }
+  function toggleRunState() {
+    isRunning = !isRunning;
+    button?.classList.toggle("pause", isRunning);
+  }
 
-    function updateClock() {
-        const now = new Date();
-        hoursElement.textContent = adjustTimer(now.getHours());
-        minutesElement.textContent = adjustTimer(now.getMinutes());
-        secondsElement.textContent = adjustTimer(now.getSeconds());
-        document.body.style.backgroundColor = rgbFromTime(
-            now.getHours(),
-            now.getMinutes(),
-            now.getSeconds()
-        );
-    }
+  function updateClock() {
+    const now = new Date();
+    hoursElement.textContent = adjustTimer(now.getHours());
+    minutesElement.textContent = adjustTimer(now.getMinutes());
+    secondsElement.textContent = adjustTimer(now.getSeconds());
+    document.body.style.backgroundColor = rgbFromTime(
+      now.getHours(),
+      now.getMinutes(),
+      now.getSeconds()
+    );
+  }
 
-    function adjustTimer(value) {
-        return value < 10 ? `0${value}` : `${value}`;
-    }
+  function adjustTimer(value) {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
 
-    function rgbFromTime(hours, minutes, seconds) {
-        const ranges = [23, 59, 59];
-        const [r, g, b] = [hours, minutes, seconds].map((value, index) =>
-            Math.round((value / ranges[index]) * 255)
-        );
-        return `rgb(${r}, ${g}, ${b})`;
-    }
+  function rgbFromTime(hours, minutes, seconds) {
+    const ranges = [23, 59, 59];
+    const [r, g, b] = [hours, minutes, seconds].map((value, index) =>
+      Math.round((value / ranges[index]) * 255)
+    );
+    return `rgb(${r}, ${g}, ${b})`;
+  }
 });
